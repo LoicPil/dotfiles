32b618c38319c28cf9434b6e0dbd783605e30e73
LoicPil <piletteloic@gmail.com>
2025-03-05 17:52:24 +0100

    Changes in emacs

--- emacs/init.el
+++ emacs/init.el
@@ -1,2 +1,19 @@
 (global-display-line-numbers-mode 1)
+(defun copy-to-clipboard ()
+  "Copy the selected region to the Wayland clipboard using wl-copy."
+  (interactive)
+  (if (region-active-p)
+      (progn
+        (shell-command-on-region (region-beginning) (region-end) "wl-copy")
+        (message "Copied to clipboard"))
+    (message "No region selected")))
+
+(global-set-key (kbd "C-c C-a") 'copy-to-clipboard)
+(defun wl-paste()
+  "Paste from the Wayland clipboard using wl-paste."
+  (shell-command-to-string "wl-paste"))
+(when (executable-find "wl-copy")
+  (setq interprogram-paste-function 'wl-paste)
+  (setq interprogram-cut-function 'wl-copy))
+
 
