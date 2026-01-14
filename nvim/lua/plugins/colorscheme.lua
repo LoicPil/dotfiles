4c3d8b71298d4cdb108b13efc52138ef652c6bfc
Pilette Loïc <piletteloic@gmail.com>
2026-01-14 22:50:27 +0100

    Add LazyVim with Solarized-Luv theme and update gitignore

---
+++ nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "Abhra00/solarized-luv.nvim",
+    lazy = false,
+    priority = 1000,
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "solarized-luv",
+    },
+  },
+}
