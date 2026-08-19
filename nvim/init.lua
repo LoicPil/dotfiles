7c4765e07829f92ebdc89a54dae2e393beabb4e5
LoicPil <piletteloic@gmail.com>
2026-08-19 11:24:17 +0200

    Modify the configuration and the Path in init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,2 +1,7 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+vim.api.nvim_create_autocmd("TermOpen", {
+  callback = function()
+    vim.env.PATH = vim.env.PATH:gsub(vim.fn.expand("~/.local/share/nvim/mason/bin") .. ":?", "")
+  end,
+})
--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -3,12 +3,23 @@ return {
     "folke/tokyonight.nvim",
     lazy = false,
     priority = 1000,
-    opts = { style = "storm" },
+    opts = {
+      style = "storm", -- ou "night", "moon", "day"
+      on_highlights = function(hl, c)
+        hl.Function = { fg = c.green }
+        hl["@function"] = { fg = c.green }
+        hl["@function.call"] = { fg = c.green }
+        hl.Keyword = { fg = c.green1 }
+        hl["@keyword"] = { fg = c.green1 }
+        hl.String = { fg = c.green2 }
+        hl["@string"] = { fg = c.green2 }
+      end,
+    },
   },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "tokyonight-storm",
+      colorscheme = "tokyonight",
     },
   },
 }
